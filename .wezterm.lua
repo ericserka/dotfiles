3f5de104ca077b1c891a7eafb2b56b086fe8e244
ericserka <ericserka@hotmail.com>
2024-08-19 19:37:24 -0300

    replacing wezterm color scheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("Fira Code Nerd Font Mono")
-config.color_scheme = "onedark"
+config.color_scheme = "Catppuccin Mocha"
 
 -- and finally, return the configuration to wezterm
 return config
