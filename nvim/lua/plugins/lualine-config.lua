661980d03ffe04d376dae93cc7040a30b1344618
ericserka <ericserka@hotmail.com>
2024-08-29 12:42:38 -0300

    add maxcount in lualine search count

--- nvim/lua/plugins/lualine-config.lua
+++ nvim/lua/plugins/lualine-config.lua
@@ -1,3 +1,10 @@
+local function search_count()
+  local count = vim.fn.searchcount({ maxcount = 100000, timeout = 500 })
+  local search_pattern = vim.fn.getreg('/')
+
+  return string.format('/%s [%d/%d]', search_pattern, count.current, count.total)
+end
+
 require("lualine").setup(
   {
     sections = {
@@ -10,7 +17,7 @@ require("lualine").setup(
       },
       lualine_x = {
         {
-          require("noice").api.status.search.get,
+          search_count,
           cond = require("noice").api.status.search.has,
           color = { fg = "#56b6c2" },
         },
