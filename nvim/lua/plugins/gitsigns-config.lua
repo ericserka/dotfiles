8f5c202d4f7ec6998275ee3ea517271bf0f3fc21
ericserka <ericserka@hotmail.com>
2025-06-17 22:16:20 -0300

    add new gitsigns remaps

--- nvim/lua/plugins/gitsigns-config.lua
+++ nvim/lua/plugins/gitsigns-config.lua
@@ -1,1 +1,4 @@
 vim.api.nvim_set_keymap('n', '<leader>hp', ":Gitsigns preview_hunk<CR>", {})
+vim.api.nvim_set_keymap('n', '<leader>hi', ":Gitsigns preview_hunk_inline<CR>", {})
+vim.api.nvim_set_keymap('n', '<leader>hs', ":Gitsigns stage_hunk<CR>", {})
+vim.api.nvim_set_keymap('n', '<leader>hS', ":Gitsigns stage_buffer<CR>", {})
