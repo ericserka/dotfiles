ec12e8465e5ffbbbd56f5da15575f623143e3be9
ericserka <ericserka@hotmail.com>
2026-03-05 07:58:27 -0300

    feat: cleaning clipboard history before start daemon

--- sway/scripts/cliphist-daemon.fish
+++ sway/scripts/cliphist-daemon.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 # cliphist-daemon
-# Start the clipboard history daemon
+# Clear previous history and start the clipboard history daemon
 
+cliphist wipe
 exec wl-paste --watch cliphist -max-items 500 -max-dedupe-search 10 store
