7f7ce981fe0eb74721d9a12676c7684e60548da1
ericserka <ericserka@hotmail.com>
2026-09-10 12:42:23 -0300

    refactor(nvim): rename the deprecated buffer keymap option to buf

--- nvim/lua/plugins/nvim-tree-config.lua
+++ nvim/lua/plugins/nvim-tree-config.lua
@@ -2,7 +2,7 @@ local function my_on_attach(bufnr)
   local api = require "nvim-tree.api"
 
   local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    return { desc = "nvim-tree: " .. desc, buf = bufnr, noremap = true, silent = true, nowait = true }
   end
 
   -- default mappings
